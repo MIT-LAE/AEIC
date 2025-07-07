aa06fef3e6d743daa74ab04e45fed9ec08c89763
prateekr9SF <119811887+prateekr9SF@users.noreply.github.com>
2025-07-07 12:00:49 -0400

    arbit commit

--- src/weather/contrails/main.py
+++ src/weather/contrails/main.py
@@ -2,10 +2,10 @@ import utils as util
 import postProcess as proc
 # working data set info: specific humidity (q), temperature (T), Pressure (p) X 100
 
-weather_grib = 'data/12-30-22.grib'
+weather_grib = 'data/3.grib'
 
 # Dataset overview 
-util.inspect_grib_fields(weather_grib)
+#util.inspect_grib_fields(weather_grib)
 
 print("PRE-PROCESSING DATA...")
 
@@ -15,25 +15,35 @@ print("PRE-PROCESSING DATA...")
 #weather = util.preprocess_by_levels(weather_grib,levels)
 
 # Loop over 24 hourly time indices
-for time_index in range(1,24):
-    print("Processing time index: {time_index}")
+for time_index in range(0,21):
+    print(f"Processing time index: {time_index}")
+    
+    weather = util.preprocess_single_time_slice(weather_grib, time_index)
+    
+    weather = util.compute_contrail_probability(weather,sigma_rhi = 3.0)
+    
+    proc.plot_issr_conus_by_altitude(weather, time_index )
 
-    try:
+   #try:
         
         # Pre-process data for a single time slice
-        weather = util.preprocess_single_time_slice(weather_grib, time_index)
+    #weather = util.preprocess_single_time_slice(weather_grib, time_index)
         
         # Compute 1D CDF using RHi
-        weather = util.compute_contrail_probability(weather,sigma_rhi = 3.0)
+    #weather = util.compute_contrail_probability(weather,sigma_rhi = 3.0)
         
+        # Plot ISSR contour onver CONUS
+    #proc.plot_issr_conus_by_altitude(weather, time_index )
         
+    #except Exception as e:
+        #print(f"Failed at time index {time_index}: {e}")
 
 
 #print(weather.head())
 
         # Plot ISSR contour onver CONUS
         #proc.plot_issr_conus_by_altitude(weather, time_index )
-        proc.plot_pcon_conus_by_altitude(weather, time_index )
-    except Exception as e:
-        print(f"Failed at time index {time_index}: {e}")
+        #proc.plot_pcon_conus_by_altitude(weather, time_index )
+    #except Exception as e:
+    #    print(f"Failed at time index {time_index}: {e}")
 #proc.save_issr_animation(weather, filename="issr_animation.mp4", fps=2)
