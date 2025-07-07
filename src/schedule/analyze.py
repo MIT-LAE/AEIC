ea621ce96648075dfd3cfadca771c4872c2b88a3
prateekr9SF <119811887+prateekr9SF@users.noreply.github.com>
2025-07-07 14:04:53 -0400

    add deg 3 polynomial

--- src/schedule/analyze.py
+++ src/schedule/analyze.py
@@ -23,14 +23,38 @@ def plot_avg_bin(df, month):
     aircraft_614_df["DISTANCE_BIN"] = pd.cut(aircraft_614_df["DISTANCE"], bins=bins)
     
     
+    
     binned = aircraft_614_df.groupby("DISTANCE_BIN", observed=False)["LOAD_FACTOR"].mean()
     binned_std = aircraft_614_df.groupby("DISTANCE_BIN", observed = False)["LOAD_FACTOR"].std()
+    
+    
+
+    
+    # Polynomial definition
+    
+    # Remove NaNs from binned mean and corresponding labels
+    valid_mask = ~np.isnan(binned.values)
+    clean_labels = labels[:len(binned)][valid_mask]
+    clean_means = binned.values[valid_mask]
+
+    degree = 3
+    coeffs = np.polyfit(clean_labels, clean_means, degree)
+    poly = np.poly1d(coeffs)
+    
+    
+    # Create smooth x values across your plotted range
+    x_fit = np.linspace(min(labels), max(labels), 500)
+    y_fit = poly(x_fit)
     
     
     
     # Create scatter plot
     fig1 = plt.figure()
     ax1 = fig1.gca()
+    
+    
+    # Plot the polynomial fit
+    plt.plot(x_fit, y_fit, color='crimson', linewidth=2, linestyle='--', label=f'{degree}° polynomial fit')
     
     
     plt.errorbar(
@@ -143,50 +167,6 @@ def plot_loess(df):
     
     plt.savefig('012024_loess_load_factor_vs_distance.png')
     
-    
-    
-def plot_raw_scatter(df, month):
-    # Filter data for aircraft type 614 and SEATS > 0 to avoid division by zero
-    aircraft_614_df = df[(df["AIRCRAFT_TYPE"] == 614) & (df["SEATS"] > 10) & (df["PASSENGERS"] > 10) & (df["MONTH"] == month) ].copy()
-
-    # Compute load factor
-    aircraft_614_df["LOAD_FACTOR"] = aircraft_614_df["PASSENGERS"] / aircraft_614_df["SEATS"]
-
-    # Create scatter plot
-    fig1 = plt.figure()
-    ax1 = fig1.gca()
-
-    plt.scatter(aircraft_614_df["DISTANCE"], aircraft_614_df["LOAD_FACTOR"], alpha=0.5, s=10)
-
-    plt.xlabel("Distance (nm)", fontsize=22, fontname="Times New Roman")
-    plt.ylabel("Load Factor", fontsize=22, fontname="Times New Roman")
-
-
-    plt.xticks(fontname="Times New Roman", fontsize=20)
-    plt.yticks(fontname="Times New Roman", fontsize=20)
-    
-    
-    ax1.tick_params(bottom=True, top=True, left=True, right=True)
-    ax1.tick_params(labelbottom=True, labeltop=False, labelleft=True, labelright=False) 
-    ax1.tick_params(which='major', length=10, width=1.2, direction='in')
-    ax1.tick_params(which='minor', length=5, width=1.2, direction='in')
-    
-    # Plot grid properties
-    ax1.grid(which='major', color='black', linestyle=':', linewidth='0.05')
-    ax1.minorticks_on()
-    ax1.grid(which='minor', color='black', linestyle=':', linewidth='0.05')
-
-    F = plt.gcf()
-    Size = F.get_size_inches()
-    F.set_size_inches(Size[0] * 1.5, Size[1] * 1.5, forward=True)
-
-    # High resolution settings
-    plt.rcParams['figure.dpi'] = 300
-    plt.rcParams['savefig.dpi'] = 300
-
-
-    plt.tight_layout()
-    plt.savefig(f"Plots/{month}_2024_load_factor_vs_distance.png")
 
 
 
@@ -194,7 +174,7 @@ def plot_raw_scatter(df, month):
 file_path = "data/T_T100D_SEGMENT_ALL_CARRIER.csv"
 df = pd.read_csv(file_path)
 
-month_index = 6
+month_index = 8
 
 plot_avg_bin(df, month_index)
 
