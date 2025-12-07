95eeee251ab370165a059cda314208171926ef71
Aditeya Shukla <aditeyashukla@gmail.com>
2025-12-07 03:14:36 -0500

    updated tests to include multipoint

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -43,6 +43,19 @@ def sample_weather(ground_track, weather_dataset_path):
     )
 
 
+@pytest.fixture(scope='session')
+def sample_weather_multipoint(ground_track, weather_dataset_path):
+    return Weather(
+        ds=str(weather_dataset_path),
+        mission=sample_mission,
+        ground_track=ground_track,
+        fl_min=10,
+        fl_max=300,
+        fl_spacing=10,
+        points_between_waypoints=100,
+    )
+
+
 def test_weather_init_without_dataset_requires_weather_data(ground_track):
     with pytest.raises(ValueError, match='Weather dataset must be provided'):
         Weather(ds=None, mission=sample_mission, ground_track=ground_track)
@@ -62,14 +75,28 @@ def test_weather_initializes_regridded_maps(sample_weather):
     assert start_u == pytest.approx(5.73948156392396, rel=1e-6)
 
 
-def test_compute_ground_speed(sample_weather):
+def test_compute_ground_speed(sample_weather, sample_weather_multipoint):
     gs, heading, wind_u, wind_v = sample_weather.compute_ground_speed(
-        ground_distance_m=10.0 * NAUTICAL_MILES_TO_METERS,
+        ground_distance_m=200.0 * NAUTICAL_MILES_TO_METERS,
         altitude_m=30000 * FEET_TO_METERS,
         tas_ms=200.0,
     )
-
-    assert gs == pytest.approx(192.24156612925196, rel=1e-6)
+    assert gs == pytest.approx(194.58943989242772, rel=1e-6)
     assert heading == pytest.approx(234.9032159733623, rel=1e-6)
-    assert wind_u == pytest.approx(15.785100100781209, rel=1e-6)
-    assert wind_v == pytest.approx(-1.0294373674107957, rel=1e-6)
+    assert wind_u == pytest.approx(12.78462411578592, rel=1e-6)
+    assert wind_v == pytest.approx(-1.949621381603165, rel=1e-6)
+
+    gs, heading, wind_u, wind_v = sample_weather_multipoint.compute_ground_speed(
+        ground_distance_m=200.0 * NAUTICAL_MILES_TO_METERS,
+        altitude_m=30000 * FEET_TO_METERS,
+        tas_ms=200.0,
+    )
+    assert gs == pytest.approx(190.8160506345428, rel=1e-6)
+    assert heading == pytest.approx(232.54310919606212, rel=1e-6)
+    assert wind_u == pytest.approx(15.777685055872261, rel=1e-6)
+    assert wind_v == pytest.approx(0.0, rel=1e-6)
+
+
+if __name__ == "__main__":
+    # Run the tests
+    pytest.main([__file__, "-v"])
