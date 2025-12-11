205ab3d36ecd07b4a720bdd5db1780e393dd94b5
Ian Ross <ian@skybluetrades.net>
2025-12-11 14:16:01 +0100

    Make weather API easier to use; tidy test organization a little

--- src/AEIC/trajectories/builders/legacy.py
+++ src/AEIC/trajectories/builders/legacy.py
@@ -6,7 +6,6 @@ from numpy.typing import NDArray
 from AEIC.missions import Mission
 from AEIC.performance_model import PerformanceModel
 from AEIC.trajectories import FlightPhase, GroundTrack, Trajectory
-from AEIC.utils.files import file_location
 from AEIC.utils.helpers import filter_order_duplicates
 from AEIC.utils.units import (
     FEET_TO_METERS,
@@ -117,15 +116,7 @@ class LegacyContext(Context):
         # Initialize weather regridding when requested.
         self.weather: Weather | None = None
         if self.options.use_weather:
-            mission_date = mission.departure.strftime('%Y%m%d')
-            weather_path = file_location(
-                f"{ac_performance.config.weather_data_dir}/{mission_date}.nc"
-            )
-            self.weather = Weather(
-                weather_data_path=weather_path,
-                mission=mission,
-                ground_track=ground_track,
-            )
+            self.weather = Weather(data_dir=ac_performance.config.weather_data_dir)
 
         # Pass information to base context class constructor.
         super().__init__(
@@ -370,7 +361,8 @@ class LegacyBuilder(Builder):
             segment_fuel = ff * segment_time
             if self.weather is not None:
                 traj.ground_speed[i] = self.weather.get_ground_speed(
-                    ground_distance=traj.ground_distance[i],
+                    time=self.mission.departure,
+                    gt_point=self.ground_track.location(traj.ground_distance[i]),
                     altitude=traj.altitude[i],
                     true_airspeed=fwd_tas,
                     azimuth=traj.azimuth[i],
