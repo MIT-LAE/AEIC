c02b697482932e86a6c403d20cce0f6e8ca4f3ac
Ian Ross <ian@skybluetrades.net>
2026-01-06 21:17:50 +0100

    Rebuild golden test for current state of trajectory simulation

--- tests/test_golden.py
+++ tests/test_golden.py
@@ -1,10 +1,10 @@
-import pytest
+# import pytest
 
 import AEIC.trajectories.builders as tb
 from AEIC.trajectories import TrajectoryStore
 
 
-@pytest.mark.skip(reason='need to recreate golden files')
+# @pytest.mark.skip(reason='need to recreate golden files')
 def test_trajectory_simulation_golden(
     test_data_dir, sample_missions, performance_model
 ):
