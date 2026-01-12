2e6246a753ebf9fd2bd6998b9d0c88d9387ce558
Ian Ross <ian@skybluetrades.net>
2026-01-12 12:18:14 +0100

    Skip golden test on MacOS

--- tests/test_golden.py
+++ tests/test_golden.py
@@ -1,10 +1,12 @@
-# import pytest
+import sys
+
+import pytest
 
 import AEIC.trajectories.builders as tb
 from AEIC.trajectories import TrajectoryStore
 
 
-# @pytest.mark.skip(reason='need to recreate golden files')
+@pytest.mark.skipif(sys.platform != 'linux', reason='Golden test only on Linux')
 def test_trajectory_simulation_golden(
     test_data_dir, sample_missions, performance_model
 ):
